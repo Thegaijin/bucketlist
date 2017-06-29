6262c7915c3004a57f407194f8ab6b68f1031662
S V <S V>
2017-06-29 14:09:10 +0300

    Adding back-to-lists js functionality on click to items page

--- app/frontend/js/create.js
+++ app/frontend/js/create.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
     $('a.account').on('click', function() {
         $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
     });
+
+    $('a.lists').on('click', function() {
+        $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
+    });
 });
